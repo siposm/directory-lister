7d508032b9cafb184f7601f8a9a470e6fbbd1e5b
Miklos Sipos <sipos.miklos@nik.uni-obuda.hu>
2019-09-02 14:01:22 +0200

    copied files

---
+++ resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,11 @@
+<hr>
+
+<div class="footer">
+    Sipos Miklos
+    <br>
+    <br>
+    <a href="http://users.nik.uni-obuda.hu/siposm" class="self-reference">/siposm</a>
+    <br>
+    <br>
+    <img src="../favicon.ico" class="footer-image">
+</div>
